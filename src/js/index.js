74fc66c426a34e3193fb0ad335f4b2ccdd1115e8
runoob <test@runoob.com>
2018-04-21 16:45:40 +0800

    更新图片

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 require(["config"], function(){
 	require(["jquery","template","boots" , "loadHF"], function($,template){
-		
+
+		/*//出现购物车
+		$(".hot-img").hover(function(){
+			$(".join_car").css({ "top": "180px", "display": "block" });;
+		},function(){
+			$(".join_car").css({ "top": "230px", "display": "none" })
+		})*/
+			
 	});
 });
