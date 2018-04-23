592a90f4cd352b1cea263487a0572bfae3326f61
runoob <test@runoob.com>
2018-04-23 21:09:51 +0800

    登录注册

--- src/js/detail.js
+++ src/js/detail.js
@@ -2,26 +2,13 @@ require(["config"], function(){
 	require(["jquery","template","boots" ,"loadHF","zoom"], function($,template){
 	
 	/*商品放大镜*/
-	$("img").elevateZoom();
-	
-	/*点击加载数据*/
-	/* $.getJSON("/mock/djjx.json",function(data){
-	  	//使用arttemplate渲染
-	  	let html = template("xqprod_temp",{products : data.res_body.products});
-	  	console.log(data)
-	  	//显示
-	  	$(".prod_xq").prepend(html);
-	  })*/
-	 
-	/* $.cookie.json = true;
-	 
-	 let _products = $.cookie("products") || [];*/
+	$("#pic_img").elevateZoom();
 	 
 	/*商品数量的加减*/
 	
 	$(".prod-count").on("click",".btn_add,.btn_mius",function(){
 		let amount = $("#count-num").val();
-		
+		console.log(amount)
 		if($(this).is(".btn_add")){
 			amount++;
 		}else{
@@ -33,7 +20,24 @@ require(["config"], function(){
 		$("#count-num").val(amount);
 	})
 	
-	/** */
+	/** 
+	 读取id值 更具id 值去读取相应的商品详情
+	 * */
+	 
+	var purl = String(location.href);    var start = purl.indexOf("?")+1;
+	var pid = Number(purl.slice(start));
+	var jsonurl = "/mock/spxq.json";
 	
+	$.getJSON("/mock/spxq1.json",function(data){
+	  	//使用arttemplate渲染
+	  	let html = template("xqprod_temp",{products : data.res_body.products});
+	  	console.log(data)
+	  	//显示
+	  	$(".prod_xq").prepend(html);
+	  })
+	/*console.log(jsonurl)
+	console.log(typeof(pid))
+	console.log(pid)
+	console.log(start)*/
 	});
 });
