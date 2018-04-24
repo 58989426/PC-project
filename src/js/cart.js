0fb709295a2f24db5560d4df78e6355f472ed6e7
runoob <test@runoob.com>
2018-04-24 17:34:31 +0800

    更新商品详情数据

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,5 +1,48 @@
 require(["config"], function(){
-	require(["jquery","template","boots" ,"loadHF"], function($,template){
+	require(["jquery","template","boots" ,"loadHF","cookie"], function($,template){
+		//从cookiez中读取购物车数据
+		$.cookie.json = true;
+		// 读取所有cookie中保存的购物车数据
+		let _products = $.cookie("products") || [];
 		
+		if (_products.length === 0) { // 没有购物车数据
+			$(".cart_empty").show()
+							.next(".cart_table").hide();
+			return;
+		}
+		// 显示读取到的 cookie 购物车数据
+		let html = template("prod_list_temp", {products: _products});
+		$(".cart_empty").hide()
+						.next(".cart_table").show()
+						.find(".product_list").html(html);
+		//商品的删除
+		$(".del").on("click",function(){
+			// 当前待删除商品的编号，在数组中的索引
+		let _id = $(this).data("id"),
+			_index = exist(_id, _products);
+		// 从数组中删除 _index 索引处的元素
+		_products.splice(_index, 1);
+		// 从cookie中移除部分数据：覆盖保存 _products 数组中的值
+		$.cookie("products", _products, {expires:7, path:"/"});
+		// 从dom树中删除节点
+		$(this).parents(".content").remove();
+
+		if (_products.length === 0) { // 没有购物车数据
+			$(".cart_empty").show()
+							.next(".cart_table").hide();
+		}
+		})
+		
+		// 找出 id 对应商品在 prodcuts 中下标
+		function exist(id, products) {
+			var existIndex = -1;
+			$.each(products, function(index, prod){
+				if(prod.pid == id) {
+					existIndex = index;
+					return false;
+				}
+			});
+			return existIndex;
+		}
 	});
 });
--- src/js/detail.js
+++ src/js/detail.js
@@ -1,7 +1,6 @@
 require(["config"], function(){
 	require(["jquery","template","boots" ,"loadHF","zoom","cookie"], function($,template){
 
-	
 	/** 
 	 读取id值 更具id 值去读取相应的商品详情
 	 * */
@@ -9,7 +8,7 @@ require(["config"], function(){
 	var purl = String(location.href);    var start = purl.indexOf("?")+1;
 	var pid = Number(purl.slice(start));
 	var jsonurl = "/mock/spxq"+pid+".json";
-	
+	console.log(pid)
 	$.getJSON(jsonurl,function(data){
 	  	//使用arttemplate渲染
 	  	let html = template("xqprod_temp",{products : data.res_body.products});
@@ -19,21 +18,42 @@ require(["config"], function(){
 	  	
 	  	/*加入购物车*/
 		$(".prod_xq").delegate("#cartBtn","click",function(e){
-			console.log("dfjkk")
-			console.log(html)
-			console.log(this)
+			let that = $(this).parent().parent().parent().parent();
+			console.log(that)
 			//将当前选购到的商品信息获取到
 			let prod = {
-				pid : $(this).find(".id").text(),
-				title : $(this).find("#cmd-title").text(),
-				price : $(this).find("#sale-price").text(),
-				img : $(this).find("img").attr("src"),
-				amount : $(this).find("#count-num").val()
+				prodid : $(that).find(".p_id").text(),
+				title : $(that).find("#cmd-title").text(),
+				price : $(that).find("#sale-price").text().slice(1),
+				img : $(that).find("img").attr("src"),
+				amount : $(that).find("#count-num").val()
 			}
 			console.log(prod)
 			//配置cookie插件
 			$.cookie.json = true;
-			//
+			//获取 cookie 中保存的 购物车
+			let products = $.cookie("products") || [];
+			// 判断原购物车中是否已存在选购商品
+			let index = exist(prod.pid, products);
+			if (index === -1) // 不存在
+				// 将当前选购商品添加到数组中保存
+				products.push(prod);
+			else // 存在
+				// 修改数量
+				products[index].amount++;
+			// 将购物车再保存回 cookie 中
+			$.cookie("products", products, {expires:7, path:"/"});
+			
+			function exist(id, products) {
+				var existIndex = -1;
+				$.each(products, function(index, prod){
+					if(prod.pid === id) {
+						existIndex = index;
+						return false;
+					}
+				});
+				return existIndex;
+			}
 	})
 		}).done(function(){   //渲染模板后
 	  		/*商品放大镜*/
